48b0501e581ca9311e0381caf73dc9ac8b7fcf2b
DiazRock <corolariodiaz@gmail.com>
2021-02-25 23:59:45 -0500

    Dinamyc routes

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 
 # Create your views here.
@@ -7,4 +7,15 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1> Hello world!</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    return HttpResponse(f"<h1> Hello {tweet_id}</h1>")
+    data = {
+        "id": tweet_id
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id= tweet_id)
+        data["content"] = obj.content
+    except:
+        data["message"] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status= status)
