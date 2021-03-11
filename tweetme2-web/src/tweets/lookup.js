53edf865ffa7112a27388620b25ddfb8cd6967cd
DiazRock <corolariodiaz@gmail.com>
2021-03-11 13:21:06 -0500

    Changed the localhost to remote url for api requests

--- tweetme2-web/src/tweets/lookup.js
+++ tweetme2-web/src/tweets/lookup.js
@@ -16,7 +16,7 @@ export function apiTweetDetail(tweetId, callback) {
 export function apiTweetFeed(callback, nextUrl) {
     let endpoint =  "/tweets/feed/"
     if (nextUrl !== null && nextUrl !== undefined) {
-        endpoint = nextUrl.replace("http://localhost:8000/api", "")
+        endpoint = nextUrl.replace("//tweetme2-diazrock.herokuapp.com/api", "")
     }
     backendLookup("GET", endpoint, callback)
 }
@@ -28,7 +28,7 @@ export function apiTweetList(username, callback, nextUrl) {
         endpoint =  `/tweets/?username=${username}`
     }
     if (nextUrl !== null && nextUrl !== undefined) {
-        endpoint = nextUrl.replace("http://localhost:8000/api", "")
+        endpoint = nextUrl.replace("//tweetme2-diazrock.herokuapp.com/api", "")
     }
     backendLookup("GET", endpoint, callback)
 }
